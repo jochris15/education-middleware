279e28857c23ee92f7e68dea13e610a73f466516
jochris15 <jochris15@gmail.com>
2024-07-25 09:51:36 +0700

    test for deployment

--- end/app.js
+++ end/app.js
@@ -1,8 +1,10 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const AuthController = require('./controllers/AuthController')
 const EventController = require('./controllers/EventController')
 const GameController = require('./controllers/GameController')
