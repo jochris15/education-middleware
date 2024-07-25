c26346203f7de0038e7dd3dfce7a776f54f03069
jochris15 <jochris15@gmail.com>
2024-07-25 10:48:43 +0700

    change back

--- end/app.js
+++ end/app.js
@@ -1,10 +1,8 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = 3000
 const AuthController = require('./controllers/AuthController')
 const EventController = require('./controllers/EventController')
 const GameController = require('./controllers/GameController')
